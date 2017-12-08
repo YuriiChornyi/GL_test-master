fbfa79d104a096c84fe74bee19432b61a1dbfa7c
Yura BLACK <yura43528@gmail.com>
2017-12-08 16:33:53 +0200

    !!

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(22, 39);
+            this.button1.Location = new System.Drawing.Point(22, 33);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(153, 23);
             this.button1.TabIndex = 0;
@@ -60,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(138, 45);
+            this.label1.Location = new System.Drawing.Point(19, 75);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 2;
